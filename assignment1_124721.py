c8424c3472dd2b1c93720a79903fd5a4388cd747
Lepiyao <levihanny_96@hotmail.com>
2022-05-04 13:35:48 +0200

    updated Py, cleaner look with explanations

--- assignment1_124721.py
+++ assignment1_124721.py
@@ -1,71 +1,70 @@
 import json
 import pandas as pd 
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import Perceptron
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
-from sklearn.datasets import load_files
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+from sklearn.metrics import classification_report
 
-print("Start 2")
+"""
+From https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
+Read json data 
+"""
 data = pd.read_json('./train.jsonl', lines=True)
-print("End 2")
+# Read the test file
+testData = pd.read_json('./test.jsonl', lines=True)
 
-print("Start 3")
+#Set data to train and test with splitting the data
 docs_train, docs_test, y_train, y_test = train_test_split(
     data.text, data.lang, test_size=0.5)
-print("End 3")
 
-print("Start 4")
-vectorizer = TfidfVectorizer(ngram_range=(1, 3), analyzer='char',
-                             use_idf=False)
-print(vectorizer)
-print("End 4")
+"""
+Use TfidfVectorizer to shorten the code length from 
+using CountVectorizer followed by TfidfTransformer
+(But I think it will took less time if we use CountVectorizer followed by TfidfTransformer)
+Set ngram_range from unigrams to trigrams
+"""
+vectorizer = TfidfVectorizer(ngram_range=(1, 3), analyzer='char', use_idf=False)
 
-print("Start 5")
+# Create the Classifier using the vectorizer
 clf = Pipeline([
     ('vec', vectorizer),
-    ('clf', Perceptron()),
+    ('clf', MultinomialNB()),
 ])
-print(clf)
-print("End 5")
 
-print("Start 6")
+# Fit the pipeline to training set
 clf.fit(docs_train, y_train)
-print("End 6")
 
-print("Start 7")
-y_predicted = clf.predict(docs_test)
-print("End 7")
+# Predict the outcome using the testing data that has been split previously
+splitPredicted = clf.predict(docs_test)
 
-print("Start 8")
-cm = metrics.confusion_matrix(y_test, y_predicted)
+#Printing Confusion Matrix and the classification report based on the split data testing
+cm = metrics.confusion_matrix(y_test, splitPredicted)
 print(cm)
-print("End 8")
+print(classification_report(y_test, splitPredicted, target_names=np.unique(y_train)))
 
-print("Start 12")
-dataTest = pd.read_json('./test.jsonl', lines=True)
-print("End 12")
+# Predict the outcome using the Test data using the .jsonl file
+predicted = clf.predict(testData.text)
 
-print("Start 13")
-predicted = clf.predict(dataTest.text)
-print("End 13")
-
-print("Start 14")
-my_dict = {}
+"""
+Creating list temporary for values
+Then using pandas to conver from Data Frame to .jsonl file
+"""
 listKey1=[]
 listKey2=[]
-for s, p in zip(dataTest.id, predicted):
-    #print('The language of "%s" is "%s"' % (s, p))
+
+for s, p in zip(testData.id, predicted):
     listKey1.append(s)
     listKey2.append(p)
 
 prediction_result = [{'id': key1, 'lang': key2} for key1, key2 in zip(listKey1, listKey2)]
+
+# From https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_json.html
 df = pd.DataFrame(prediction_result)
 result = df.to_json(orient="records")
 parsed = json.loads(result)
 
 with open('./predictions.jsonl', 'w', encoding='utf-8') as c:
     json.dump(parsed, c, ensure_ascii=False, indent=4)
-
-print("End 14")
